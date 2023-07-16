ea0d28c8b26055a84bcb52df5fb90d94b8f0d773
Osioyea <131928742+Osioyea@users.noreply.github.com>
2023-07-16 18:04:07 -0500

    Add color changing functionality.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+function setGrid(size) {
+  for(i = 0; i < Math.pow(size,2); i++){
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    let tileSize = 720/size;
+    tile.style.width = tileSize.toString() + "px";
+    tile.style.height = tileSize.toString() + "px";
+    tile.addEventListener('mouseover', () => {
+      tile.classList.add('colored');
+    });
+    container.appendChild(tile);
+  }
+}
+
+function killGrid(){
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
+}
+
 const container = document.querySelector('.container');
 let gridSize = 16;
-for(i = 0; i < Math.pow(gridSize,2); i++){
-  let tile = document.createElement('div');
-  tile.classList.add('tile');
-  let tileSize = 720/gridSize;
-  tile.style.width = tileSize.toString() + "px";
-  tile.style.height = tileSize.toString() + "px";
-  container.appendChild(tile);
-}
+
+setGrid(gridSize);
